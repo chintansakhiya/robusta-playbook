2804298c579e67e8526f2a917b457e7fdb7741bf
chintan sakhiya <123356373+chintansakhiya@users.noreply.github.com>
2024-06-20 11:20:08 +0530

    Update cronjob.py

--- cronjob/cronjob.py
+++ cronjob/cronjob.py
@@ -22,7 +22,6 @@ def get_cronjob(event: JobEvent):
     """
     # job=event
     cluster=event._context
-    print(cluster)
     details=get_all_cronjob_details()
     block_list: List[BaseBlock] = []
     if len(details.items)!=0:
